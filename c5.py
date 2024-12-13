a103fb03e2aea3bc1229efe8d8c5586a6549640a
sayed-Glink <sayed.webit@gmail.comm>
2024-12-13 21:18:32 +0600

    add new py

---
+++ c5.py
@@ -0,0 +1,137 @@
+# def myfunction():
+#     print('my name is abu sayed')
+
+
+# x=('helo world')
+# print(x)
+# def my_function():
+#     print("Hello from a function") 
+# def my_function(fname, lname):
+#     print(fname + " " + lname)
+# my_function("abu")
+     
+# def my_function(fname, lname):
+#  print(fname + " " + lname)
+# my_function("Emil", "Refsnes")
+# def my_function(fname, lname):
+#     print(fname + ' ' + lname)
+# my_function('abu', 'sayed')
+# def myf(fname, lname):
+#     print(fname + ' ' + lname)
+# myf('abu', 'sayed')
+
+# def my_function(*kids):
+#     print("The youngest child is " + kids[2])
+# my_function("Emil", "Tobias", "Linus")
+# def my_function(*kuds):
+#     print('the youngest child ' + kuds [2])
+# my_function('abu', 'saued', 'lablu', 'rioinjn')
+# def my_function(*name):
+#     print('the select name is ' + name[2])
+# my_function('lablu', 'abu', 'sayed', 'riopn')
+
+# def my_function(*kids):
+#     print("The youngest child is " + kids[2])
+# my_function("Emil", "Tobias", "Linus")
+# def my_function(*kids):
+   
+#     print("The youngest child is " + kids[2])
+# str=('hello world in python')
+# x=1
+# print(x)
+# print(type
+# x,y,z='asds', 'dasd', 'sdd'
+# print(x)
+# print(y)
+# print(z)
+# x=y=z= 'orange'
+# print(x)
+# print(y)
+# print(z)
+# group = ['orange','banana','apple']
+# x, y, z = group
+
+# print(x,y,z)
+
+# def myfunction(fname , lname):
+# #     print(fname  +' '+  lname)
+# # myfunction(fname='abu', lname='sayed')
+# def myfunction(fname, lname):
+#     print(fname+ ' ' +lname)
+# myfunction(fname='abu',lname='sayed')
+
+# def myfunction(*best):
+#     print('the best name is ' + best[2])
+# myfunction('abu', 'sayed', 'lablu','sdrtre')
+
+# def myfunction(c1, c2,c3):
+#     print('the best name is ' + c3)
+# myfunction(c1='abu', c2='sayed', c3='sayma')
+# ________________________________________________good___________________
+# def smaller_num():
+#     x=int(input('Enter the first number: '))
+#     y=int(input('Enter the second number: '))
+#     if x<y:
+#         return x
+#     else:
+#         return y
+# print('The smaller number is: ',smaller_num())
+# ________________________________________________good___________________
+
+# def my_function(*value):
+#     print('my name is : ' + value[2])
+# my_function('abu', 'sayed', 'lablu')
+
+# def my_function(child1, child2, child3):
+#     print('youngest name is :' + child3)
+# my_function(child3='abu', child1='sayed', child2='lablu')
+
+# def my_function(fname):
+#    print(fname + " Refsnes")
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
+# def my_function(fname, lname):
+#     print(fname + " " + lname)
+# my_function("Emil", "Refsnes")
+
+# def my_function(fname, lname):
+#     print(fname + ' ' + lname)
+# my_function('this is your_name: ' 'abu', 'sayed')
+
+# x=10
+# print(type(x))
+# print(x)
+
+# x=10.5
+# print(type(x))
+# print(x)
+
+# x=2j+232
+# x=1234245+7j
+# print(type(x))
+# print(x)
+
+# import random
+
+# print(random.randrange(1, 10))
+
+# __________input
+
+# usename=input("enter your name:")
+# passWord=input("enter your password:")
+# email=input("enter your email:")
+# print(usename)
+# print(passWord)
+# print(email)
+x=2
+y=3
+print(x+y)
+
+
+
+
+
+
+
+
